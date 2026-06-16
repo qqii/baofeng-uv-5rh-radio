47f74eeada0e1d3541e45ff39e7cf5f66be359e3
qqii <QiruiChen97@gmail.com>
2026-06-16 19:12:24 +0100

    chore: configure lefthook scripts

---
+++ .config/lefthook/commit-msg/conventional-commit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -uo pipefail
+trap 'status=$?; echo "$0: Error on line $LINENO: $BASH_COMMAND"; exit "$status"' ERR
+IFS=$'\n\t' # nosemgrep: bash.lang.security.ifs-tampering.ifs-tampering
+
+# Validate the commit message follows Conventional Commits.
+# https://www.conventionalcommits.org/
+
+read -r line < "$1" || true
+[[ $line =~ ^(feat|fix|refactor|perf|style|test|docs|build|ops|chore)(\(.+\))?!?:\ .+ ]] \
+  || [[ $line =~ ^(fixup|squash|amend|wip)\! ]]
